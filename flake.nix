df4a5c5b52907c93537de319a4de4f4b49e5aaaa
Abdennour Zahaf <zfnori@gmail.com>
2024-12-15 18:16:11 +0100

    refactor: remove home manager & updates

--- flake.nix
+++ flake.nix
@@ -3,29 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = {
-    nixpkgs,
-    home-manager,
-    ...
-  }: {
+  outputs = {nixpkgs, ...}: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "bak";
-          home-manager.users.abdennour = import ./home.nix;
-        }
-      ];
+      modules = [./configuration.nix];
     };
   };
 }
